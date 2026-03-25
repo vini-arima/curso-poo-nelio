d1ed415d5f0df4eb67b270fa5479aaeb8d30bfd3
Vinícius Cardoso <viniciuscarimateia@gmail.com>
2026-03-24 21:27:30 -0300

    Update Retangulo.java

--- M8 - Exercicio - Retangulo/entidade/Retangulo.java
+++ M8 - Exercicio - Retangulo/entidade/Retangulo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Retangulo {
     double Width;
     double Height;
@@ -12,5 +14,18 @@ public class Retangulo {
 
     public double diagonal() {
         return Math.sqrt((Width * Width) + (Height * Height));
+    }
+}
+public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Retangulo figura = new Retangulo();
+
+        System.out.println("Enter rectangle width and height: \n");
+        figura.Width = scan.nextDouble();
+        figura.Height = scan.nextDouble();
+
+        System.out.printf("AREA = %.2f PERIMETER = %.2f DIAGONAL = %.2f%n",
+                figura.area(), figura.perimetro(), figura.diagonal());
     }
 }
